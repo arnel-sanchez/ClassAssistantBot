d2f019abe89f8966f5e9b1a0bf19001248151789
Arnel Sanchez Rodríguez <56517197+arnel-sanchez@users.noreply.github.com>
2022-10-19 17:48:18 -0400

    Update Program.cs

--- ClassAssistantBot/Program.cs
+++ ClassAssistantBot/Program.cs
@@ -5,13 +5,6 @@ using ClassAssistantBot.Services;
 var text = System.IO.File.ReadAllText("./environment.json");
 var configuration = JsonSerializer.Deserialize<Configuration>(text);
 DataAccess dataAccess = new DataAccess();
-var excercises = dataAccess.Excercises.ToList();
-foreach (var excercise in excercises)
-{
-    var credits = dataAccess.Credits.Where(x => x.ObjectId == excercise.Id).ToList();
-    dataAccess.RemoveRange(credits);
-}
-dataAccess.SaveChanges();
 Console.WriteLine("Start!");
 Console.WriteLine("Telegram API Key:" + configuration.TelegramApiKey);
 Engine.StartPolling(dataAccess, configuration.TelegramApiKey);
